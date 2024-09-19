723064ae4b8d79ba69420d60e420794114e804f4
Taofiq Abdulsalam <156853865+WBLT4U@users.noreply.github.com>
2024-09-19 15:17:40 -0700

    Update InnerLayout.tsx

--- src/component/layout/InnerLayout.tsx
+++ src/component/layout/InnerLayout.tsx
@@ -1,17 +1,1 @@
-import React from "react";
-import FooterSection3 from "../footer/FooterSection3";
-import HeaderSection4 from "../header/HeaderSection4";
-type Props = {
-  children: React.ReactNode;
-};
-const InnerLayout = ({ children }: Props) => {
-  return (
-    <>
-      <HeaderSection4 />
-      {children}
-      <FooterSection3 />
-    </>
-  );
-};
 
-export default InnerLayout;
